7c8400e679112ae8c998b8df30f83d01b6e03636
Gabi Thaler <gmth054@gmail.com>
2025-08-28 16:55:59 +0300

    didline1

--- Enricher/sub_antisemitic.py
+++ Enricher/sub_antisemitic.py
@@ -7,7 +7,7 @@ from weapon import WeaponProcessor
 from producer import Producer
 
 consumer = KafkaConsumer(
-    'raw_tweets_antisemitic',
+    'preprocessed_tweets_antisemitic',
     bootstrap_servers='localhost:9092',
     group_id='my-group',
     value_deserializer=lambda x: json.loads(x.decode('utf-8')),
