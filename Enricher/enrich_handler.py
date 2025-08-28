b3d25e404662255f7191d598093c679918f4ddee
Eli <elibloch2903@gmail.com>
2025-08-28 12:27:58 +0300

    סידור של הקבצים פלוס שליחה לקפקא בלי חילוץ תאריך

--- Enricher/enrich_handler.py
+++ Enricher/enrich_handler.py
@@ -1,15 +1,23 @@
 class EnrichHandler:
-    def __init__(self, sentiment_processor, weapons_detector=None):
+    def __init__(self, sentiment_processor, weapons_detector=None, producer=None, output_topic=None):
         self.sp = sentiment_processor
         self.wp = weapons_detector
+        self.producer = producer
+        self.output_topic = output_topic
 
     def handle(self, message: dict) -> dict:
         clean_text = message.get("clean_text", "")
 
+        # enrich
         message["sentiment"] = self.sp.get_sentiment(clean_text)
-        message["weapons_detected"] = (
-            self.wp.find_weapons(clean_text) if self.wp else []
-        )
+        message["weapons_detected"] = self.wp.find_weapons(clean_text) if self.wp else []
 
-        print(message)
+        # publish (אם הוגדר)
+        if self.producer and self.output_topic:
+            key = message.get("id")  # לא חובה, שומר סדר per key
+            self.producer.publish(self.output_topic, message, key=key)
+        else:
+            print(message)
+
         return message
+
--- Enricher/sub_not_antisemitic.py
+++ Enricher/sub_not_antisemitic.py
@@ -1,9 +1,10 @@
 import json
 from kafka import KafkaConsumer
-from Enricher.message_manager import MessageManager
-from Enricher.sentiment import SentimentProcessor
-from Enricher.enrich_handler import EnrichHandler
-from weapon import WeaponProcessor  # ← חדש
+from message_manager import MessageManager
+from sentiment import SentimentProcessor
+from enrich_handler import EnrichHandler
+from weapon import WeaponProcessor
+from producer import Producer
 
 consumer = KafkaConsumer(
     'preprocessed_tweets_not_antisemitic',
@@ -14,10 +15,13 @@ consumer = KafkaConsumer(
 
 sp = SentimentProcessor()
 wp = WeaponProcessor("./data/weapon_list.txt")
+prod = Producer()
 
 handler = EnrichHandler(
     sentiment_processor=sp,
-    weapons_detector=wp
+    weapons_detector=wp,
+    producer=prod,
+    output_topic='enriched_preprocessed_tweets_not_antisemitic'
 )
 
 manager = MessageManager(consumer, handler)
