5cbc057431701215ddb14e9ab1deccabab447311
Eli <elibloch2903@gmail.com>
2025-08-28 12:00:13 +0300

    סידור של הקבצים

--- Enricher/enrich_handler.py
+++ Enricher/enrich_handler.py
@@ -1,14 +1,14 @@
 class EnrichHandler:
-    def __init__(self, sentiment_processor, weapons_detector=None):
+    def __init__(self, sentiment_processor, weapons_detector):
+        if weapons_detector is None:
+            raise ValueError("weapons_detector is required")
         self.sp = sentiment_processor
         self.wp = weapons_detector
 
     def handle(self, message: dict) -> dict:
         clean_text = message.get("clean_text", "")
-
         message["sentiment"] = self.sp.get_sentiment(clean_text)
 
-        if self.wp:
-            message["weapons_detected"] = self.wp.find_weapon(clean_text)
+        message["weapons_detected"] = self.wp.find_weapon(clean_text) or ""
         print(message)
         return message
